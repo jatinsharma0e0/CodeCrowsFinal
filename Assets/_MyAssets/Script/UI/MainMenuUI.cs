c44462542fb99d0f2441c3f0c870fda80e53afd3
jatinsharma0e0 <177534118+jatinsharma0e0@users.noreply.github.com>
2026-01-05 20:44:36 +0530

    Town added

---
+++ Assets/_MyAssets/Script/UI/MainMenuUI.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace MR.Game.UI
+{
+    public class MainMenuUI : MonoBehaviour
+    {
+        [Header("Buttons")]
+        public Button startButton;
+        public Button quitButton;
+
+        [Header("Scene Settings")]
+        [SerializeField] private string gameSceneName = "GameScene";
+        void Start()
+        {
+            startButton.onClick.AddListener(PlayGame);
+            quitButton.onClick.AddListener(QuitGame);
+        }
+
+
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(gameSceneName);
+
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("Game quit");
+            Application.Quit();
+        }
+    }
+}
